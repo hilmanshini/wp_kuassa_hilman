b4de2733f6edfb93bd3210e4b07d5dbbfbbe6427
Abdul Malik Ikhsan <samsonasik@gmail.com>
2013-02-13 11:48:05 +0700

    wp_kuassa first commit

---
+++ wp-content/plugins/dig_banner/dig_banner.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Dig Banner
+Plugin URI: http://samsonasik.wordpress.com
+Description: Diginiq Banner
+Version: 1.0
+License: GPLv2
+Author: Abdul Malik Ikhsan
+Author URI: http://samsonasik.wordpress.com
+*/
+
+function showBannerIn($page)
+{
+    global $wpdb;
+    
+    
+}
